3dd6e5bf0c3f4567c0714b503f12348981f2528f
KeetonMartin <klmartin@haverford.edu>
2022-07-09 14:47:34 -0700

    Style, and filter by city functionality

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
 global_data = {}
+city1s = {}
+
+function getCity1s() {
+  $.ajax({
+    url: "https://data.transportation.gov/resource/4f3n-jbg2.json",
+    type: "GET",
+    data: {
+      "$select": "city1",
+      "$group": "city1",
+      "$order": "city1",
+      "$limit": 5000
+    } 
+  }).done(function (cities) {
+    // console.log(cities);
+    // city1s = cities;
+    output = "[";
+    for (var i = 0, len = cities.length; i < len; ++i) {
+      output+= "\"" + cities[i]["city1"] + "\",";
+    }
+    output+= "]"
+    console.log(output)
+  })
+  return city1s;
+}
 
 function findFlights() {
   $.ajax({
     url: "https://data.transportation.gov/resource/4f3n-jbg2.json",
     type: "GET",
     data: {
-      "$limit": 50,
+      "$where": "year > 2018",
+      "$limit": 5000,
+      "$order": "passengers DESC",
       "$$app_token": "Qto9G2rlKlEYzT0U1Kb6RzJLj"
     }
   }).done(function (data) {
     global_data = data
 
-    var html = '<table><thead><tr>...</tr></thead><tbody>';
+    city1selection = document.getElementById("city1").value;
+    console.log("City1 selection: " + city1selection);
+
+    var html = '<table class="table"><thead><tr></tr></thead><tbody>';
+    
+    html += "<th scope=\"col\">Largest Carrier</th><th scope=\"col\">City 1</th><th scope=\"col\">City 2</th><th scope=\"col\">Largest Carrier Avg Fare</th><th scope=\"col\">Year</th><th scope=\"col\">Quarter</th>"
+
     for (var i = 0, len = global_data.length; i < len; ++i) {
+
+      // console.log("City1 selection: " + city1selection + " vs. curr: " + global_data[i]["city1"]);
+      if (city1selection != global_data[i]["city1"] && city1selection != global_data[i]["city2"] && city1selection!="") { continue; }
+
       html += '<tr>';
       // for (var j = 0, rowLen = global_data[i].length; j < rowLen; ++j) {
       //   html += '<td>' + global_data[i][j] + '</td>';
@@ -21,11 +57,15 @@ function findFlights() {
       html += '<td>' + global_data[i]["city1"] + '</td>';
       html += '<td>' + global_data[i]["city2"] + '</td>';
       html += '<td>' + global_data[i]["fare_lg"] + '</td>';
+      html += '<td>' + global_data[i]["year"] + '</td>';
+      html += '<td>' + global_data[i]["quarter"] + '</td>';
+
   
       html += "</tr>";
     }
-    html += '</tbody><tfoot><tr>....</tr></tfoot></table>';
+    html += '</tbody><tfoot><tr></tr></tfoot></table>';
   
+    $( ".output" ).empty();
     $(html).appendTo('#output');
   })
   
