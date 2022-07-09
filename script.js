b8be09044af146900197266cffbac86f56693079
KeetonMartin <klmartin@haverford.edu>
2022-07-08 17:04:03 -0700

    added functioning button, and should work on web to find flights

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
 global_data = {}
 
-$.ajax({
-  url: "https://data.transportation.gov/resource/4f3n-jbg2.json",
-  type: "GET",
-  data: {
-    "$limit": 50,
-    "$$app_token": "Qto9G2rlKlEYzT0U1Kb6RzJLj"
-  }
-}).done(function (data) {
-  alert("Retrieved " + data.length + " records from the dataset!");
-  console.log(data);
-  global_data = data
+function findFlights() {
+  $.ajax({
+    url: "https://data.transportation.gov/resource/4f3n-jbg2.json",
+    type: "GET",
+    data: {
+      "$limit": 50,
+      "$$app_token": "Qto9G2rlKlEYzT0U1Kb6RzJLj"
+    }
+  }).done(function (data) {
+    global_data = data
 
-  var html = '<table><thead><tr>...</tr></thead><tbody>';
-  for (var i = 0, len = global_data.length; i < len; ++i) {
-    html += '<tr>';
-    // for (var j = 0, rowLen = global_data[i].length; j < rowLen; ++j) {
-    //   html += '<td>' + global_data[i][j] + '</td>';
-    // }
-    html += '<td>' + global_data[i]["carrier_lg"] + '</td>';
-    html += '<td>' + global_data[i]["city1"] + '</td>';
-    html += '<td>' + global_data[i]["city2"] + '</td>';
-    html += '<td>' + global_data[i]["fare_lg"] + '</td>';
+    var html = '<table><thead><tr>...</tr></thead><tbody>';
+    for (var i = 0, len = global_data.length; i < len; ++i) {
+      html += '<tr>';
+      // for (var j = 0, rowLen = global_data[i].length; j < rowLen; ++j) {
+      //   html += '<td>' + global_data[i][j] + '</td>';
+      // }
+      html += '<td>' + global_data[i]["carrier_lg"] + '</td>';
+      html += '<td>' + global_data[i]["city1"] + '</td>';
+      html += '<td>' + global_data[i]["city2"] + '</td>';
+      html += '<td>' + global_data[i]["fare_lg"] + '</td>';
+  
+      html += "</tr>";
+    }
+    html += '</tbody><tfoot><tr>....</tr></tfoot></table>';
+  
+    $(html).appendTo('#output');
+  })
+  
+}
 
-    html += "</tr>";
-  }
-  html += '</tbody><tfoot><tr>....</tr></tfoot></table>';
+// $.ajax({
+//   url: "https://data.transportation.gov/resource/4f3n-jbg2.json",
+//   type: "GET",
+//   data: {
+//     "$limit": 50,
+//     "$$app_token": "Qto9G2rlKlEYzT0U1Kb6RzJLj"
+//   }
+// }).done(function (data) {
+//   alert("Retrieved " + data.length + " records from the dataset!");
+//   console.log(data);
+//   global_data = data
 
-  $(html).appendTo('#output');
+//   var html = '<table><thead><tr>...</tr></thead><tbody>';
+//   for (var i = 0, len = global_data.length; i < len; ++i) {
+//     html += '<tr>';
+//     // for (var j = 0, rowLen = global_data[i].length; j < rowLen; ++j) {
+//     //   html += '<td>' + global_data[i][j] + '</td>';
+//     // }
+//     html += '<td>' + global_data[i]["carrier_lg"] + '</td>';
+//     html += '<td>' + global_data[i]["city1"] + '</td>';
+//     html += '<td>' + global_data[i]["city2"] + '</td>';
+//     html += '<td>' + global_data[i]["fare_lg"] + '</td>';
+
+//     html += "</tr>";
+//   }
+//   html += '</tbody><tfoot><tr>....</tr></tfoot></table>';
+
+//   $(html).appendTo('#output');
 
 
 
-});
+// });
 
